4f41658e90e09f39079a2dcd8d6a13e00eb22c9d
LinesGuy <60029482+LinesGuy@users.noreply.github.com>
2021-09-01 18:29:35 +0100

    Add virtual tag to Entity.HandleTilemapCollision()

--- Ether/Entity.cs
+++ Ether/Entity.cs
@@ -26,7 +26,7 @@ namespace Mono_Ether.Ether
             spriteBatch.Draw(Image, screenPos, null, Color, Orientation, Size / 2f, Camera.Zoom, 0, 0);
         }
 
-        public void HandleTilemapCollision()
+        public virtual void HandleTilemapCollision()
         {
             var tile = Map.GetTileFromWorld(Position);
 
--- Ether/PauseMenu.cs
+++ Ether/PauseMenu.cs
@@ -1,7 +1,13 @@
-﻿namespace Mono_Ether.Ether
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mono_Ether.Ether
 {
     public static class PauseMenu
     {
-        
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Art.pauseBg, GameRoot.ScreenSize / 2f, null, Color.White, 0f, Art.pauseBg.Size() / 2f, 1f, SpriteEffects.None, 0);
+        }
     }
 }
