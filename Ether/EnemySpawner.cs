6aff2657779359ced429d98a86384762f33163fa
LinesGuy <60029482+LinesGuy@users.noreply.github.com>
2021-09-05 21:06:10 +0100

    Enable EnemySpawner by default and add optional args to GetSpawnPosition

--- Ether/EnemySpawner.cs
+++ Ether/EnemySpawner.cs
@@ -8,7 +8,7 @@ namespace Mono_Ether.Ether
     {
         static Random _rand = new Random();
         static float _inverseSpawnChance = 60;
-        public static bool enabled = false; // DISABLED BY DEFAULT FOR DEBUGGING
+        public static bool enabled = true;
         public static void Update()
         {
             if (!enabled)
@@ -34,24 +34,24 @@ namespace Mono_Ether.Ether
                 _inverseSpawnChance -= 0.005f;
         }
 
-        public static Vector2 GetSpawnPosition()
+        public static Vector2 GetSpawnPosition(float radius = 500f, int attempts = 10)
         {
             // If returns Vector2.Zero, could not find valid spawn position
             Vector2 pos;
             Vector2 playerPos = PlayerShip.Instance.Position;
-            int remainingAttempts = 10;
+            int remainingAttempts = attempts;
             do
             {
-                pos = new Vector2(_rand.NextFloat(playerPos.X - 500, playerPos.X + 500), _rand.NextFloat(playerPos.Y - 500, playerPos.Y + 500));
+                pos = new Vector2(_rand.NextFloat(playerPos.X - radius, playerPos.X + radius), _rand.NextFloat(playerPos.Y - radius, playerPos.Y + radius));
                 remainingAttempts -= 1;
             }
-            while ((Vector2.DistanceSquared(pos, PlayerShip.Instance.Position) < 250 * 250
+            while ((Vector2.DistanceSquared(pos, PlayerShip.Instance.Position) < Math.Pow(radius / 2f, 2)
                    || Map.GetTileFromMap(Map.WorldtoMap(pos)).TileId > 0
                    || pos.X < 0 || pos.Y < 0 || pos.X > Map._size.X * Map.cellSize || pos.Y > Map._size.Y * Map.cellSize)
                    && remainingAttempts > 0);
             if (remainingAttempts == 0)
             {
-                Debug.WriteLine("Could not find spawn position after 10 attempts, skipping");
+                Debug.WriteLine($"Could not find spawn position after {attempts} attempts, skipping");
                 return Vector2.Zero;
             }
             return pos;
