e67a5b27396dd4b371da6d94e798b3c84a954339
LinesGuy <60029482+LinesGuy@users.noreply.github.com>
2021-09-19 11:43:21 +0100

    (WIP) add other screens

--- Ether/PauseMenu.cs
+++ Ether/PauseMenu.cs
@@ -5,6 +5,7 @@ namespace Mono_Ether.Ether
 {
     public static class PauseMenu
     {
+        private static MenuButtonManager buttonManager = new MenuButtonManager();
         public static void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(Art.pauseBg, GameRoot.ScreenSize / 2f, null, Color.White, 0f, Art.pauseBg.Size() / 2f, 1f, SpriteEffects.None, 0);
---
+++ MainMenu/CreditsScreen.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mono_Ether.MainMenu
+{
+    public class CreditsScreen : States.GameState
+    {
+        private MenuButtonManager menuButtonManager;
+        public CreditsScreen(GraphicsDevice graphicsDevice) : base(graphicsDevice)
+        {
+        }
+        public override void Initialize()
+        {
+            menuButtonManager = new MenuButtonManager();
+            menuButtonManager.Add("back");
+        }
+        public override void LoadContent(ContentManager content)
+        {
+            //throw new NotImplementedException();
+        }
+        public override void UnloadContent()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            var clickedButton = menuButtonManager.getClickedButton();
+            if (clickedButton == "back")
+                GameRoot.Instance.RemoveScreen();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
+            spriteBatch.DrawString(Art.DebugFont, "everything by me", Vector2.Zero, Color.White);
+            menuButtonManager.Draw(spriteBatch);
+            spriteBatch.End();
+        }
+    }
+}
