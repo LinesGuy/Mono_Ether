b1b4d5a3e43e3ab986f65482030ea8b895d7dbad
linus <gudgeon.christopher@gmail.com>
2021-10-08 18:05:33 +0100

    add LastMouse to Input.cs

--- Input.cs
+++ Input.cs
@@ -4,12 +4,13 @@ using MonoGame.Extended.Input;
 namespace Mono_Ether {
     static class Input {
         public static KeyboardStateExtended Keyboard;
+        public static MouseStateExtended LastMouse;
         public static MouseStateExtended Mouse;
-
         public static Vector2 MousePosition { get { return new Vector2(Mouse.X, Mouse.Y); } }
 
         public static void Update() {
             Keyboard = KeyboardExtended.GetState();
+            LastMouse = Mouse;
             Mouse = MouseExtended.GetState();
         }
         public static Vector2 GetMovementDirection() {
