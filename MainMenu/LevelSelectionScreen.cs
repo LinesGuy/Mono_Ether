2e044aac6a242991a35d4014acef89e9c7cbabd4
linus <gudgeon.christopher@gmail.com>
2021-10-08 18:06:10 +0100

    button revamp xddxd

--- MainMenu/LevelSelectionScreen.cs
+++ MainMenu/LevelSelectionScreen.cs
@@ -3,17 +3,19 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
 namespace Mono_Ether.MainMenu {
     public class LevelSelectionScreen : States.GameState {
         private ButtonManager buttonManager;
+        private float buttonOffsetVelocity = 0f;
         public LevelSelectionScreen(GraphicsDevice graphicsDevice) : base(graphicsDevice) {
         }
         public override void Initialize() {
             buttonManager = new ButtonManager();
-            buttonManager.AddButton(new LevelButton(0, "Testing stage"));
+            buttonManager.AddButton(new LevelButton(0, "Test stage"));
             buttonManager.AddButton(new LevelButton(1, "Tutorial"));
             buttonManager.AddButton(new LevelButton(2, "Level one"));
             buttonManager.AddButton(new LevelButton(3, "Placeholder A"));
@@ -43,19 +45,33 @@ namespace Mono_Ether.MainMenu {
                     GameRoot.Instance.AddScreen(new Ether.EtherRoot(GameRoot.Instance.graphicsasdfasdfasdf));
                     Ether.Map.LoadFromFile("Tutorial.txt", new Vector2(32, 32));
                     Ether.BackgroundParticleManager.Populate(Ether.Map.WorldSize, 256);
-
                     break;
                 case "Level one":
                     break;
                 default:
                     break;
             }
+            if (Input.Mouse.WasButtonJustUp(MonoGame.Extended.Input.MouseButton.Left))
+                buttonOffsetVelocity = (Input.Mouse.Y - Input.LastMouse.Y) / LevelButton.RADIUS;
+            if (Input.Mouse.IsButtonDown(MonoGame.Extended.Input.MouseButton.Left))
+            {
+                foreach(LevelButton button in buttonManager.Buttons)
+                    button.Offset -= (Input.Mouse.Y - Input.LastMouse.Y) / LevelButton.RADIUS;
+            }
+            else
+            {
+                foreach (LevelButton button in buttonManager.Buttons)
+                    button.Offset -= buttonOffsetVelocity;
+                buttonOffsetVelocity /= 1.1f;
+            }
+            
+
         }
 
         public override void Draw(SpriteBatch spriteBatch) {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-            spriteBatch.DrawString(Art.DebugFont, "levels xd", Vector2.Zero, Color.White);
+            spriteBatch.DrawString(Art.DebugFont, "Level selection screen", Vector2.Zero, Color.White);
             buttonManager.Draw(spriteBatch);
             spriteBatch.End();
         }
