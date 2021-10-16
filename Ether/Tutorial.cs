b5f90a9bdcaa6cdd5b09bd090567318c42233f49
linus <gudgeon.christopher@gmail.com>
2021-10-16 14:32:05 +0100

    Extended tutorial

--- Ether/Tutorial.cs
+++ Ether/Tutorial.cs
@@ -18,8 +18,29 @@ namespace Mono_Ether.Ether {
                         Input.Keyboard.WasKeyJustDown(Microsoft.Xna.Framework.Input.Keys.A) ||
                         Input.Keyboard.WasKeyJustDown(Microsoft.Xna.Framework.Input.Keys.S) ||
                         Input.Keyboard.WasKeyJustDown(Microsoft.Xna.Framework.Input.Keys.D)) {
-                        state = "shooting";
+                        state = "camera";
                     }
+                    break;
+                case "camera":
+                    tutorialText = "Use arrow keys to move the camera around";
+                    if (Input.Keyboard.WasKeyJustDown(Microsoft.Xna.Framework.Input.Keys.Left) ||
+                        Input.Keyboard.WasKeyJustDown(Microsoft.Xna.Framework.Input.Keys.Right) ||
+                        Input.Keyboard.WasKeyJustDown(Microsoft.Xna.Framework.Input.Keys.Up) ||
+                        Input.Keyboard.WasKeyJustDown(Microsoft.Xna.Framework.Input.Keys.Down)) {
+                        state = "zooming";
+                    }
+                    break;
+                case "zooming":
+                    tutorialText = "Use Q and E or scroll wheel to zoom";
+                    if (Input.Keyboard.WasKeyJustDown(Microsoft.Xna.Framework.Input.Keys.Q) ||
+                        Input.Keyboard.WasKeyJustDown(Microsoft.Xna.Framework.Input.Keys.E) ||
+                        Input.Mouse.DeltaScrollWheelValue != 0)
+                        state = "disableLerp";
+                    break;
+                case "disableLerp":
+                    tutorialText = "Press C to disable free camera";
+                    if (Input.Keyboard.WasKeyJustDown(Microsoft.Xna.Framework.Input.Keys.C))
+                        state = "shooting";
                     break;
                 case "shooting":
                     tutorialText = "Move the cursor around and hold left click to shoot";
@@ -28,12 +49,17 @@ namespace Mono_Ether.Ether {
                         EnemySpawner.enabled = true;
                         timer = 10f;
                     }
-
                     break;
                 case "shootingEnemies":
                     tutorialText = $"Enemy spawning is now enabled. Please survive {timer:0} more seconds.";
                     timer -= 0.0166f;
                     if (timer <= 0)
+                        state = "powerpacks";
+                    break;
+                case "powerpacks":
+                    tutorialText = "Power pack spawning is enabled. Move over a power pack to pick it up";
+                    PowerPackSpawner.enabled = true;
+                    if (PlayerShip.Instance.activePowerPacks.Count > 0)
                         state = "starburst";
                     break;
                 case "starburst":
