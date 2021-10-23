9a983b3d26a649694cb10ddc0239a30ae3a03195
linus <gudgeon.christopher@gmail.com>
2021-10-23 13:21:01 +0100

    Add GameSettings.ApplyChanges()

--- GameSettings.cs
+++ GameSettings.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
 using Mono_Ether.States;
 using System;
 using System.Collections.Generic;
 
 namespace Mono_Ether {
     public static class GameSettings {
-        public static float MasterVolume = 1.0f;
-        public static float MusicVolume = 0.1f;
-        public static float SoundEffectVolume = 0.1f;
+        public static float MasterVolume = 0.05f;
+        public static float MusicVolume = 0.5f;
+        public static float SoundEffectVolume = 0.8f;
+        public static void ApplyChanges() {
+            SoundEffect.MasterVolume = GameSettings.MasterVolume * GameSettings.SoundEffectVolume;
+            MediaPlayer.Volume = GameSettings.MasterVolume * GameSettings.MusicVolume;
+        }
     }
 }
