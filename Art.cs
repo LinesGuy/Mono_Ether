a3de26ee7b16baff02a185c0583927f75069dbf2
linus <gudgeon.christopher@gmail.com>
2021-10-23 11:02:03 +0100

    so good

--- Art.cs
+++ Art.cs
@@ -4,105 +4,109 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Mono_Ether {
     static class Art {
-        public static Texture2D Pixel { get; private set; }
-        public static Texture2D Default { get; private set; }
-        public static Texture2D Player { get; private set; }
-        public static Texture2D GreenSeeker { get; private set; }
-        public static Texture2D PinkSeeker { get; private set; }
-        public static Texture2D PinkSeekerChild { get; private set; }
-        public static Texture2D BlueSeeker { get; private set; }
-        public static Texture2D PurpleWanderer { get; private set; }
-        public static Texture2D PinkWanderer { get; private set; }
-        public static Texture2D Bullet { get; private set; }
-        public static Texture2D StarBurst { get; private set; }
-        public static Texture2D Pointer { get; private set; }
-        public static Texture2D LineParticle { get; private set; }
-        public static Texture2D Glow { get; private set; }
-        public static SoundEffect PlayerShoot { get; private set; }
-        public static SoundEffect PlayerDeath { get; private set; }
-        public static SoundEffect EnemyExplosion { get; private set; }
-        public static SpriteFont Arial24 { get; private set; }
-        public static SpriteFont NovaSquare24 { get; private set; }
-        public static SpriteFont NovaSquare48 { get; private set; }
-        public static Texture2D WelcomeText { get; private set; }
-        public static Texture2D MenuButtonBlank { get; private set; }
-        public static Texture2D TileGrass { get; private set; }
-        public static Texture2D TileDirt { get; private set; }
-        public static Texture2D TileStone { get; private set; }
-        public static Texture2D TileSus { get; private set; }
-        public static Texture2D CollisionLeft { get; private set; }
-        public static Texture2D CollisionUp { get; private set; }
-        public static Texture2D CollisionRight { get; private set; }
-        public static Texture2D CollisionDown { get; private set; }
-        public static Texture2D CollisionTopLeft { get; private set; }
-        public static Texture2D CollisionTopRight { get; private set; }
-        public static Texture2D CollisionBottomRight { get; private set; }
-        public static Texture2D CollisionBottomLeft { get; private set; }
-        public static Texture2D PauseBg { get; private set; }
-        public static Texture2D PauseExit { get; private set; }
-        public static Texture2D PauseResume { get; private set; }
-        public static Texture2D PowerShootSpeedIncrease { get; private set; }
-        public static Texture2D PowerShootSpeedDecrease { get; private set; }
-        public static Texture2D PowerMoveSpeedIncrease { get; private set; }
-        public static Texture2D PowerMoveSpeedDecrease { get; private set; }
-        public static SoundEffect PowerPackPickup { get; private set; }
-        public static SoundEffect PowerPackPickupBad { get; private set; }
-        public static Texture2D BackgroundParticle { get; private set; }
-        public static Texture2D Heart { get; private set; }
-        public static Texture2D SnakeHead { get; private set; }
-        public static Texture2D SnakeBody { get; private set; }
-        public static Texture2D BackAndForther { get; private set; }
-        public static Texture2D Geom { get; private set; }
+        public static SpriteFont Arial24;
+        public static SpriteFont NovaSquare24;
+        public static SpriteFont NovaSquare48;
+
+        public static Texture2D Glow;
+        public static Texture2D Geom;
+        public static Texture2D Heart;
+        public static Texture2D Pixel;
+        public static Texture2D Player;
+        public static Texture2D Bullet;
+        public static Texture2D PauseBg;
+        public static Texture2D Default;
+        public static Texture2D TileSus;
+        public static Texture2D Pointer;
+        public static Texture2D TileDirt;
+        public static Texture2D SnakeBody;
+        public static Texture2D PauseExit;
+        public static Texture2D SnakeHead;
+        public static Texture2D StarBurst;
+        public static Texture2D TileGrass;
+        public static Texture2D TileStone;
+        public static Texture2D BlueSeeker;
+        public static Texture2D PinkSeeker;
+        public static Texture2D GreenSeeker;
+        public static Texture2D PauseResume;
+        public static Texture2D CollisionUp;
+        public static Texture2D WelcomeText;
+        public static Texture2D PinkWanderer;
+        public static Texture2D LineParticle;
+        public static Texture2D CollisionLeft;
+        public static Texture2D CollisionDown;
+        public static Texture2D BackAndForther;
+        public static Texture2D PurpleWanderer;
+        public static Texture2D CollisionRight;
+        public static Texture2D PinkSeekerChild;
+        public static Texture2D MenuButtonBlank;
+        public static Texture2D CollisionTopLeft;
+        public static Texture2D CollisionTopRight;
+        public static Texture2D BackgroundParticle;        
+        public static Texture2D CollisionBottomLeft;
+        public static Texture2D CollisionBottomRight;
+        public static Texture2D PowerMoveSpeedIncrease;
+        public static Texture2D PowerMoveSpeedDecrease;
+        public static Texture2D PowerShootSpeedIncrease;
+        public static Texture2D PowerShootSpeedDecrease;
+
+        public static SoundEffect PlayerShoot;
+        public static SoundEffect PlayerDeath;
+        public static SoundEffect EnemyExplosion;
+        public static SoundEffect PowerPackPickup;
+        public static SoundEffect PowerPackPickupBad;
         public static void Load(ContentManager content) {
-            Pixel = content.Load<Texture2D>("Textures/Menu/pixel");
-            Default = content.Load<Texture2D>("Textures/Gameplay/Default");
-            Player = content.Load<Texture2D>("Textures/Gameplay/Player");
-            BlueSeeker = content.Load<Texture2D>("Textures/Gameplay/BlueSeeker");
-            GreenSeeker = content.Load<Texture2D>("Textures/Gameplay/GreenSeeker");
-            PinkSeeker = content.Load<Texture2D>("Textures/Gameplay/PinkSeeker");
-            PinkSeekerChild = content.Load<Texture2D>("Textures/Gameplay/PinkSeekerChild");
-            PinkWanderer = content.Load<Texture2D>("Textures/Gameplay/PinkWanderer");
-            PurpleWanderer = content.Load<Texture2D>("Textures/Gameplay/PurpleWanderer");
-            Bullet = content.Load<Texture2D>("Textures/Gameplay/Bullet");
-            StarBurst = content.Load<Texture2D>("Textures/Gameplay/StarBurst");
-            Pointer = content.Load<Texture2D>("Textures/Gameplay/Pointer");
-            LineParticle = content.Load<Texture2D>("Textures/Gameplay/Laser");
-            Glow = content.Load<Texture2D>("Textures/Gameplay/Glow");
-            PlayerShoot = content.Load<SoundEffect>("Samples/Gameplay/player_shoot");
-            PlayerDeath = content.Load<SoundEffect>("Samples/Gameplay/player_death");
-            EnemyExplosion = content.Load<SoundEffect>("Samples/Gameplay/enemy_explosion");
             Arial24 = content.Load<SpriteFont>("Fonts/Arial24");
             NovaSquare24 = content.Load<SpriteFont>("Fonts/NovaSquare24");
             NovaSquare48 = content.Load<SpriteFont>("Fonts/NovaSquare48");
-            WelcomeText = content.Load<Texture2D>("Textures/Intro/welcome_text");
-            MenuButtonBlank = content.Load<Texture2D>("Textures/Menu/menu_button_blank");
-            TileGrass = content.Load<Texture2D>("Textures/tiles/tile1");
-            TileDirt = content.Load<Texture2D>("Textures/tiles/tile2");
-            TileStone = content.Load<Texture2D>("Textures/tiles/tile3");
+
+            Pixel = content.Load<Texture2D>("Textures/Menu/pixel");
+            Glow = content.Load<Texture2D>("Textures/Gameplay/Glow");
+            Geom = content.Load<Texture2D>("Textures/GamePlay/Geom");
             TileSus = content.Load<Texture2D>("Textures/tiles/tile4");
-            CollisionLeft = content.Load<Texture2D>("Textures/tiles/collisionLeft");
-            CollisionUp = content.Load<Texture2D>("Textures/tiles/collisionUp");
-            CollisionRight = content.Load<Texture2D>("Textures/tiles/collisionRight");
-            CollisionDown = content.Load<Texture2D>("Textures/tiles/collisionDown");
-            CollisionTopLeft = content.Load<Texture2D>("Textures/tiles/collisionTopLeft");
-            CollisionTopRight = content.Load<Texture2D>("Textures/tiles/collisionTopRight");
-            CollisionBottomRight = content.Load<Texture2D>("Textures/tiles/collisionBottomRight");
-            CollisionBottomLeft = content.Load<Texture2D>("Textures/tiles/collisionBottomLeft");
-            PauseBg = content.Load<Texture2D>("Textures/GamePause/pause_bg");
-            PauseExit = content.Load<Texture2D>("Textures/GamePause/exit_button");
-            PauseResume = content.Load<Texture2D>("Textures/GamePause/resume_button");
-            PowerShootSpeedIncrease = content.Load<Texture2D>("Textures/GamePlay/PowerShootSpeedIncrease");
-            PowerShootSpeedDecrease = content.Load<Texture2D>("Textures/GamePlay/PowerShootSpeedDecrease");
-            PowerMoveSpeedIncrease = content.Load<Texture2D>("Textures/GamePlay/PowerMoveSpeedIncrease");
-            PowerMoveSpeedDecrease = content.Load<Texture2D>("Textures/GamePlay/PowerMoveSpeedDecrease");
-            PowerPackPickup = content.Load<SoundEffect>("Samples/Gameplay/PowerPackPickup");
-            PowerPackPickupBad = content.Load<SoundEffect>("Samples/Gameplay/PowerPackPickupBad");
-            BackgroundParticle = content.Load<Texture2D>("Textures/GamePlay/BackgroundParticle");
             Heart = content.Load<Texture2D>("Textures/GamePlay/Heart");
+            TileDirt = content.Load<Texture2D>("Textures/tiles/tile2");
+            TileGrass = content.Load<Texture2D>("Textures/tiles/tile1");
+            TileStone = content.Load<Texture2D>("Textures/tiles/tile3");
+            Player = content.Load<Texture2D>("Textures/Gameplay/Player");
+            Bullet = content.Load<Texture2D>("Textures/Gameplay/Bullet");
+            Default = content.Load<Texture2D>("Textures/Gameplay/Default");
+            Pointer = content.Load<Texture2D>("Textures/Gameplay/Pointer");
+            PauseBg = content.Load<Texture2D>("Textures/GamePause/pause_bg");
+            LineParticle = content.Load<Texture2D>("Textures/Gameplay/Laser");
             SnakeHead = content.Load<Texture2D>("Textures/GamePlay/SnakeHead");
             SnakeBody = content.Load<Texture2D>("Textures/GamePlay/SnakeBody");
+            StarBurst = content.Load<Texture2D>("Textures/Gameplay/StarBurst");
+            CollisionUp = content.Load<Texture2D>("Textures/tiles/collisionUp");
+            BlueSeeker = content.Load<Texture2D>("Textures/Gameplay/BlueSeeker");
+            PinkSeeker = content.Load<Texture2D>("Textures/Gameplay/PinkSeeker");
+            WelcomeText = content.Load<Texture2D>("Textures/Intro/welcome_text");
+            PauseExit = content.Load<Texture2D>("Textures/GamePause/exit_button");
+            GreenSeeker = content.Load<Texture2D>("Textures/Gameplay/GreenSeeker");
+            CollisionDown = content.Load<Texture2D>("Textures/tiles/collisionDown");
+            CollisionLeft = content.Load<Texture2D>("Textures/tiles/collisionLeft");
+            PinkWanderer = content.Load<Texture2D>("Textures/Gameplay/PinkWanderer");
+            CollisionRight = content.Load<Texture2D>("Textures/tiles/collisionRight");
+            PauseResume = content.Load<Texture2D>("Textures/GamePause/resume_button");
             BackAndForther = content.Load<Texture2D>("Textures/GamePlay/BackAndForther");
-            Geom = content.Load<Texture2D>("Textures/GamePlay/Geom");
+            MenuButtonBlank = content.Load<Texture2D>("Textures/Menu/menu_button_blank");
+            PurpleWanderer = content.Load<Texture2D>("Textures/Gameplay/PurpleWanderer");
+            CollisionTopLeft = content.Load<Texture2D>("Textures/tiles/collisionTopLeft");
+            PinkSeekerChild = content.Load<Texture2D>("Textures/Gameplay/PinkSeekerChild");
+            CollisionTopRight = content.Load<Texture2D>("Textures/tiles/collisionTopRight");
+            CollisionBottomLeft = content.Load<Texture2D>("Textures/tiles/collisionBottomLeft");
+            BackgroundParticle = content.Load<Texture2D>("Textures/GamePlay/BackgroundParticle");
+            CollisionBottomRight = content.Load<Texture2D>("Textures/tiles/collisionBottomRight");
+            PowerMoveSpeedIncrease = content.Load<Texture2D>("Textures/GamePlay/PowerMoveSpeedIncrease");
+            PowerMoveSpeedDecrease = content.Load<Texture2D>("Textures/GamePlay/PowerMoveSpeedDecrease");
+            PowerShootSpeedIncrease = content.Load<Texture2D>("Textures/GamePlay/PowerShootSpeedIncrease");
+            PowerShootSpeedDecrease = content.Load<Texture2D>("Textures/GamePlay/PowerShootSpeedDecrease");
+
+            PlayerShoot = content.Load<SoundEffect>("Samples/Gameplay/player_shoot");
+            PlayerDeath = content.Load<SoundEffect>("Samples/Gameplay/player_death");
+            EnemyExplosion = content.Load<SoundEffect>("Samples/Gameplay/enemy_explosion");
+            PowerPackPickup = content.Load<SoundEffect>("Samples/Gameplay/PowerPackPickup");
+            PowerPackPickupBad = content.Load<SoundEffect>("Samples/Gameplay/PowerPackPickupBad");
         }
     }
 }
