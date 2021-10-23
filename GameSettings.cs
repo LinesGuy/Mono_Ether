253f995ca1f800f9aed51b59223b0afd244a00f9
linus <gudgeon.christopher@gmail.com>
2021-10-23 11:45:38 +0100

    Add GameSettings.cs

---
+++ GameSettings.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Mono_Ether.States;
+using System;
+using System.Collections.Generic;
+
+namespace Mono_Ether {
+    public static class GameSettings {
+        public static float MasterVolume = 1.0f;
+        public static float MusicVolume = 0.1f;
+        public static float SoundEffectVolume = 0.1f;
+    }
+}
