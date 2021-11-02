67e98e0a587fa73401738d99aa42c58094f0613b
linus <gudgeon.christopher@gmail.com>
2021-11-02 17:10:18 +0000

    add difficulty setting to gamesettings

--- GameSettings.cs
+++ GameSettings.cs
@@ -8,9 +8,10 @@ namespace Mono_Ether {
         public static float MasterVolume;
         public static float MusicVolume;
         public static float SoundEffectVolume;
+        public static string Difficulty;
         public static void ApplyChanges() {
-            SoundEffect.MasterVolume = GameSettings.MasterVolume * GameSettings.SoundEffectVolume;
-            MediaPlayer.Volume = GameSettings.MasterVolume * GameSettings.MusicVolume;
+            SoundEffect.MasterVolume = MasterVolume * SoundEffectVolume;
+            MediaPlayer.Volume = MasterVolume * MusicVolume;
         }
         public static void LoadSettings() {
             if (File.Exists(settingsFilename)) {
@@ -18,15 +19,16 @@ namespace Mono_Ether {
                 float.TryParse(lines[0], out MasterVolume);
                 float.TryParse(lines[1], out MusicVolume);
                 float.TryParse(lines[2], out SoundEffectVolume);
+                Difficulty = lines[3];
             } else {
                 // Default settings
-                File.WriteAllText(settingsFilename, "0.05\n0.05\n0.8");
+                File.WriteAllText(settingsFilename, "0.05\n0.05\n0.8\nEasy");
                 LoadSettings();
             }
-            GameSettings.ApplyChanges();
+            ApplyChanges();
         }
         public static void SaveSettings() {
-            File.WriteAllText(settingsFilename, $"{MasterVolume}\n{MusicVolume}\n{SoundEffectVolume}");
+            File.WriteAllText(settingsFilename, $"{MasterVolume}\n{MusicVolume}\n{SoundEffectVolume}\n{Difficulty}");
         }
     }
 }
