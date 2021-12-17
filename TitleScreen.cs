0fcff2ffcbf8780d15ac4a2e9de6f3ca788bc36e
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-12-17 09:57:29 +0000

    Adjust titlescreen to different screen sizes

--- TitleScreen.cs
+++ TitleScreen.cs
@@ -32,13 +32,13 @@ namespace Mono_Ether {
             _rand = new Random();
             /* Create buttonManager and add Start/Exit buttons */
             buttonManager = new ButtonManager();
-            buttonManager.Buttons.Add(new Button(new Vector2(GameSettings.ScreenSize.X / 2f - 300f, GameSettings.ScreenSize.Y - 75f), "Start"));
-            buttonManager.Buttons.Add(new Button(new Vector2(GameSettings.ScreenSize.X / 2f + 300f, GameSettings.ScreenSize.Y - 75f), "Exit"));
+            buttonManager.Buttons.Add(new Button(new Vector2(GameSettings.ScreenSize.X / 2f - 300f, GameSettings.ScreenSize.Y - 80f), "Start"));
+            buttonManager.Buttons.Add(new Button(new Vector2(GameSettings.ScreenSize.X / 2f + 300f, GameSettings.ScreenSize.Y - 80f), "Exit"));
             /* Create lists of small/big stars with random positions */
             _smallStars = new List<Vector2>();
             _bigStars = new List<Vector2>();
             for (var i = 0; i < 150; i++)
-                _smallStars.Add(new Vector2(_rand.Next(0, (int)GameSettings.ScreenSize.X), _rand.Next(0, (int)(GameSettings.ScreenSize.Y / 1.3f))));
+                _smallStars.Add(new Vector2(_rand.Next(0, (int)GameSettings.ScreenSize.X), _rand.Next(0, (int)(GameSettings.ScreenSize.Y))));
             for (var i = 0; i < 5; i++)
                 _bigStars.Add(new Vector2(_rand.Next(0, (int)GameSettings.ScreenSize.X), _rand.Next(0, (int)GameSettings.ScreenSize.Y)));
         }
@@ -82,15 +82,15 @@ namespace Mono_Ether {
         public override void Draw(SpriteBatch batch) {
             GraphicsDevice.Clear(Color.MediumPurple);
             /* Bg */
-            batch.Draw(_bg, Vector2.Zero, Color.White);
+            batch.Draw(_bg, MyUtils.RectangleF(0, 0, GameSettings.ScreenSize.X, GameSettings.ScreenSize.Y), Color.White);
             /* Triangles */
             for (int i = 0; i < 5; i++)
-                batch.Draw(_triangle, new Vector2(GameSettings.ScreenSize.X / 2f, GameSettings.ScreenSize.Y / 1.4f), null, GetTransparentColor(255 / (i / 2 + 1)), MathF.Sin(_frame / 250f + i * 10) / 5f, new Vector2(_triangle.Width / 2f, _triangle.Height), 1f + MathF.Sin(_frame / 260f + i * 17) / 10f, 0, 0);
+                batch.Draw(_triangle, GameSettings.ScreenSize / 2f + new Vector2(0f, 161f), null, GetTransparentColor(255 / (i / 2 + 1)), MathF.Sin(_frame / 250f + i * 10) / 5f, new Vector2(_triangle.Width / 2f, _triangle.Height), 1f + MathF.Sin(_frame / 260f + i * 17) / 10f, 0, 0);
             /* SubBars */
             for (int i = 0; i < 6; i++)
-                batch.Draw(_subBar, new Vector2(GameSettings.ScreenSize.X / 2f, GameSettings.ScreenSize.Y / 1.3f + MathF.Exp((_frame + i * 20) % 120 / 16f)), null, Color.White, 0f, _subBar.Size() / 2f, 1f, 0, 0);
+                batch.Draw(_subBar, new Vector2(GameSettings.ScreenSize.X / 2f, GameSettings.ScreenSize.Y - 203f + MathF.Exp((_frame + i * 20) % 120 / 16f)), null, Color.White, 0f, _subBar.Size() / 2f, 1f, 0, 0);
             /* MainBar */
-            batch.Draw(_mainBar, new Vector2(GameSettings.ScreenSize.X / 2f, GameSettings.ScreenSize.Y / 1.3f), null, Color.White, 0f, _mainBar.Size() / 2f, 1f, 0, 0);
+            batch.Draw(_mainBar, new Vector2(GameSettings.ScreenSize.X / 2f,  GameSettings.ScreenSize.Y - 203f), null, Color.White, 0f, _mainBar.Size() / 2f, 1f, 0, 0);
             /* SmallStars */
             foreach (var starPos in _smallStars)
                 batch.Draw(_smallStar, starPos, null, GetTransparentColor(0.5f + MathF.Sin(starPos.X + _frame / 60f) * 0.5f), 0f, _smallStar.Size() / 2f, 1f + MathF.Sin(starPos.X) / 3f, 0, 0);
@@ -98,15 +98,15 @@ namespace Mono_Ether {
             foreach (var starPos in _bigStars)
                 batch.Draw(_bigStar, starPos, null, GetTransparentColor(0.5f + MathF.Sin(starPos.X + _frame / 60f) * 0.5f), starPos.X + _frame / 60f, _bigStar.Size() / 2f, 0.5f + MathF.Sin(starPos.X) / 3f, 0, 0);
             /* EtherOut (Blue -> Pink -> White) */
-            batch.Draw(_etherOutline, new Vector2(GameSettings.ScreenSize.X / 2f, GameSettings.ScreenSize.Y / 2.3f) + new Vector2(-6f, 4f) * MathF.Sin(_frame / 135f), null, Color.CornflowerBlue, MathF.Sin(_frame / 150f) / 16f, _etherOutline.Size() / 2f, 1.5f + MathF.Sin(_frame / 110f) / 20f, 0, 0);
-            batch.Draw(_etherOutline, new Vector2(GameSettings.ScreenSize.X / 2f, GameSettings.ScreenSize.Y / 2.3f) + new Vector2(6f, -4f) * MathF.Sin(_frame / 135f), null, Color.Violet, MathF.Sin(_frame / 150f) / 16f, _etherOutline.Size() / 2f, 1.5f + MathF.Sin(_frame / 110f) / 20f, 0, 0);
-            batch.Draw(_etherOutline, new Vector2(GameSettings.ScreenSize.X / 2f, GameSettings.ScreenSize.Y / 2.3f), null, Color.White, MathF.Sin(_frame / 150f) / 16f, _etherOutline.Size() / 2f, 1.5f + MathF.Sin(_frame / 110f) / 20f, 0, 0);
+            batch.Draw(_etherOutline, GameSettings.ScreenSize / 2f + new Vector2(-6f, -48f), null, Color.CornflowerBlue, MathF.Sin(_frame / 150f) / 16f, _etherOutline.Size() / 2f, 1.5f + MathF.Sin(_frame / 110f) / 20f, 0, 0);
+            batch.Draw(_etherOutline, GameSettings.ScreenSize / 2f + new Vector2(6f, -56f), null, Color.Violet, MathF.Sin(_frame / 150f) / 16f, _etherOutline.Size() / 2f, 1.5f + MathF.Sin(_frame / 110f) / 20f, 0, 0);
+            batch.Draw(_etherOutline, GameSettings.ScreenSize / 2f + new Vector2(0f, -52f), null, Color.White, MathF.Sin(_frame / 150f) / 16f, _etherOutline.Size() / 2f, 1.5f + MathF.Sin(_frame / 110f) / 20f, 0, 0);
             /* Ether */
-            batch.Draw(_etherText, new Vector2(GameSettings.ScreenSize.X / 2f, GameSettings.ScreenSize.Y / 2.3f), null, Color.White, MathF.Sin(_frame / 150f) / 16f, _etherText.Size() / 2f, 1.5f + MathF.Sin(_frame / 110f) / 20f, 0, 0);
+            batch.Draw(_etherText, GameSettings.ScreenSize / 2f + new Vector2(0f, -52f), null, Color.White, MathF.Sin(_frame / 150f) / 16f, _etherText.Size() / 2f, 1.5f + MathF.Sin(_frame / 110f) / 20f, 0, 0);
             /* Mono */
-            batch.Draw(_mono, new Vector2(GameSettings.ScreenSize.X / 2f, GameSettings.ScreenSize.Y / 5f), null, Color.White, MathF.Sin(_frame / 120f) / 20f, _mono.Size() / 2f, 1.5f + MathF.Sin(_frame / 130f) / 20f, 0, 0);
+            batch.Draw(_mono, GameSettings.ScreenSize / 2f + new Vector2(0f, -231f), null, Color.White, MathF.Sin(_frame / 120f) / 20f, _mono.Size() / 2f, 1.5f + MathF.Sin(_frame / 130f) / 20f, 0, 0);
             /* ByChris */
-            batch.Draw(_byChris, new Vector2(GameSettings.ScreenSize.X / 2f, GameSettings.ScreenSize.Y / 1.5f), null, Color.White, MathF.Sin(_frame / 90f) / 30f, _byChris.Size() / 2f, 1.5f + MathF.Sin(_frame / 80f) / 20f, 0, 0);
+            batch.Draw(_byChris, GameSettings.ScreenSize / 2f + new Vector2(0f, 125f), null, Color.White, MathF.Sin(_frame / 90f) / 30f, _byChris.Size() / 2f, 1.5f + MathF.Sin(_frame / 80f) / 20f, 0, 0);
             /* Buttons */
             buttonManager.Draw(batch);
             /* Flash screen on start, keep this below everything else */
