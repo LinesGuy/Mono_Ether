0f6db94ed96266c474187dca3ab1b70ea34e2c34
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-12-17 09:19:52 +0000

    ScreenManager rewrite

--- ScreenManager.cs
+++ ScreenManager.cs
@@ -1,80 +1,22 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Mono_Ether.States;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace Mono_Ether
-{
-    public static class ScreenManager
-    {
-        public static Stack<GameState> screenStack = new Stack<GameState>();
-        public static GameState pendingScreen;
-        public static int framesUntilTransition = 0;
-        public static int transitionState = 0; // 0 = none, 1 = load screen, -1 = unload screen
-        private const int transitionLength = 30; // frames
-        public static void Update()
-        {
-            if (framesUntilTransition > 0)
-            {
-                framesUntilTransition -= 1;
-                if (framesUntilTransition == 0)
-                {
-                    if (transitionState == 1)
-                    {
-                        AddScreen(pendingScreen);
-                    }
-                    else if (transitionState == -1)
-                    {
-                        RemoveScreen();
-                    }
-                    transitionState = 0;
-                }
-            }
-            else if (framesUntilTransition < 0)
-                framesUntilTransition += 1;
+namespace Mono_Ether {
+    public static class ScreenManager {
+        private static Stack<GameState> _screenStack = new Stack<GameState>();
+        public static GameState CurrentScreen => _screenStack.Peek();
+        public static void AddScreen(GameState screen) {
+            _screenStack.Push(screen);
+            CurrentScreen.Initialize();
+            CurrentScreen.LoadContent(GameRoot.Instance.Content);
         }
-        public static void Draw(SpriteBatch spriteBatch)
+        /*private static void RemoveScreen()
         {
-            if (framesUntilTransition != 0)
-            {
-                float transparency;
-                if (framesUntilTransition > 0)
-                    transparency = 1 - framesUntilTransition / (float)transitionLength;
-                else
-                    transparency = Math.Abs(framesUntilTransition / (float)transitionLength);
-
-                spriteBatch.Draw(Art.Pixel, new Rectangle(0, 0, (int)GameRoot.ScreenSize.X, (int)GameRoot.ScreenSize.Y), Color.Black * transparency);
-            }
-        }
-        public static void AddScreen(GameState screen)
-        {
-            screenStack.Push(screen);
-            screenStack.Peek().Initialize();
-            screenStack.Peek().LoadContent(GameRoot.Instance.Content);
-            framesUntilTransition = -transitionLength;
-        }
-        public static void TransitionScreen(GameState screen)
-        {
-            pendingScreen = screen;
-            transitionState = 1;
-            framesUntilTransition = transitionLength;
-        }
-        private static void RemoveScreen()
-        {
-            screenStack.Peek().UnloadContent();
-            screenStack.Pop();
-            if (screenStack.Count == 0)
+            CurrentScreen.UnloadContent();
+            _screenStack.Pop();
+            if (_screenStack.Count == 0)
             {
                 GameRoot.Instance.Exit();
             }
-            framesUntilTransition = -transitionLength;
-        }
-        public static void RemoveScreenTransition()
-        {
-            transitionState = -1;
-            framesUntilTransition = transitionLength;
-        }
+        }*/
     }
 }
