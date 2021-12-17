6a8716cb44e6daf38b5c6e87f32a1878cfd3fc1c
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-12-17 15:39:49 +0000

    add DrawStringCentered

--- MyUtils.cs
+++ MyUtils.cs
@@ -5,5 +5,6 @@ namespace Mono_Ether {
     internal static class MyUtils {
         public static Rectangle RectangleF(float x, float y, float width, float height) => new Rectangle((int)x, (int)y, (int)width, (int)height);
         public static Vector2 Size(this Texture2D texture) => new Vector2(texture.Width, texture.Height);
+        public static void DrawStringCentered(this SpriteBatch batch, SpriteFont font, string text, Vector2 position, Color color) => batch.DrawString(font, text, position, color, 0f, font.MeasureString(text) / 2f, 1f, 0, 0);
     }
 }
