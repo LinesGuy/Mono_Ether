17bbe50a3c4b9941d5bb3c045803f4c050627981
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-12-17 09:18:09 +0000

    Merge Art.cs, Sounds.cs, Fonts.cs into GlobalAssets.cs

--- GlobalAssets.cs
+++ GlobalAssets.cs
@@ -2,12 +2,14 @@
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Mono_Ether {
-    static class Fonts {
-        public static SpriteFont Arial24;
+    static class GlobalAssets {
+        public static Texture2D Button;
+        public static Texture2D Pixel;
         public static SpriteFont NovaSquare24;
         public static SpriteFont NovaSquare48;
-        public static void Load(ContentManager content) {
-            Arial24 = content.Load<SpriteFont>("Fonts/Arial24");
+        public static void LoadContent(ContentManager content) {
+            Button = content.Load<Texture2D>("Textures/GlobalAssets/Button");
+            Pixel = content.Load<Texture2D>("Textures/GlobalAssets/Pixel");
             NovaSquare24 = content.Load<SpriteFont>("Fonts/NovaSquare24");
             NovaSquare48 = content.Load<SpriteFont>("Fonts/NovaSquare48");
         }
