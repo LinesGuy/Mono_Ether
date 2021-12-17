553432288d92098ef971cfff70a8ffcf0b0bfd1d
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-12-17 09:18:59 +0000

    Cleanup GameRoot.cs

--- GameRoot.cs
+++ GameRoot.cs
@@ -1,76 +1,59 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Mono_Ether.States;
-using System;
-using System.Collections.Generic;
 
 namespace Mono_Ether {
     public class GameRoot : Game {
         public static GameRoot Instance { get; private set; }
-        public static Vector2 ScreenSize = new Vector2(1366, 768);
-        private readonly GraphicsDeviceManager graphics;
-        public GraphicsDevice myGraphics;
-        private SpriteBatch spriteBatch;
-        public bool DebugMode = false;
+        public readonly GraphicsDeviceManager Graphics;
+        private SpriteBatch _batch;
         public GameRoot() {
             Instance = this;
-            graphics = new GraphicsDeviceManager(this);
+            Graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            // Disable V-sync:
-            /*graphics.SynchronizeWithVerticalRetrace = false;
-            IsFixedTimeStep = false;*/
         }
         protected override void Initialize() {
+            /* Allow user to resize the window */
             Window.AllowUserResizing = true;
-            Window.ClientSizeChanged += OnResize;
-            
-            graphics.PreferredBackBufferWidth = (int)ScreenSize.X;
-            graphics.PreferredBackBufferHeight = (int)ScreenSize.Y;
-            graphics.ApplyChanges();
-
+            Window.ClientSizeChanged += GameSettings.OnScreenResize;
+            /* Load game settings from .txt file */
             GameSettings.LoadSettings();
             base.Initialize();
         }
         protected override void LoadContent() {
-            myGraphics = GraphicsDevice;
-            Art.Load(Content);
-            Sounds.Load(Content);
-            Fonts.Load(Content);
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            ScreenManager.AddScreen(new MainMenu.TitleScreen(GraphicsDevice));
-            if (DebugMode) {
-                // Skip straight to testing stage
-                ScreenManager.AddScreen(new Ether.EtherRoot(GraphicsDevice, "debugMap.txt"));
-            }
+            _batch = new SpriteBatch(GraphicsDevice);
+            /* Load assets used in more than one screen */
+            GlobalAssets.LoadContent(Content);
+            /* Add Title screen to screen stack */
+            ScreenManager.AddScreen(new TitleScreen(GraphicsDevice));
+            /* If debug mode is enabled, skip straight to the testing stage */
+            // TODO implement
+            //if (GameSettings.DebugMode)
+            //ScreenManager.AddScreen(new Ether.EtherRoot(GraphicsDevice, "debugMap.txt"));
         }
 
         protected override void UnloadContent() {
+            /* This is empty as this is only ever called when the game is closed */
         }
 
         protected override void Update(GameTime gameTime) {
             Input.Update();
-            // Handle any ongoing screen transitions
-            ScreenManager.Update();
-            if (ScreenManager.screenStack.Count > 0)
-                ScreenManager.screenStack.Peek().Update(gameTime);
+            ScreenManager.CurrentScreen.Update(gameTime);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime) {
-            spriteBatch.Begin();
-            ScreenManager.screenStack.Peek().Draw(spriteBatch);
-            // Draw transition fade
-            ScreenManager.Draw(spriteBatch);
-            // Draw FPS
-            spriteBatch.DrawString(Fonts.NovaSquare24, $"FPS: {(int)(1 / gameTime.ElapsedGameTime.TotalSeconds)}", new Vector2(GameRoot.ScreenSize.X - 200, GameRoot.ScreenSize.Y - 40), Color.White);
-            spriteBatch.End();
+            _batch.Begin();
+            /* Draw current screen */
+            ScreenManager.CurrentScreen.Draw(_batch);
+            /* Draw FPS (if enabled) */
+            if (GameSettings.ShowFPS) {
+                var Font = GlobalAssets.NovaSquare24;
+                var Text = $"{(int)(1 / gameTime.ElapsedGameTime.TotalSeconds)}FPS";
+                _batch.DrawString(Font, Text, GameSettings.ScreenSize - Font.MeasureString(Text) - new Vector2(10f, 5f), Color.White);
+            }
+            _batch.End();
             base.Draw(gameTime);
-        }
-        public void OnResize(Object sender, EventArgs e)
-        {
-            ScreenSize.X = Window.ClientBounds.Width;
-            ScreenSize.Y = Window.ClientBounds.Height;
         }
     }
 }
