b9246ea5be4a4a6c2b8ebf23e2b6aa27dd1c9b6b
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-12-18 14:26:50 +0000

    Add SliderType enum and SliderTypeToName

--- MyUtils.cs
+++ MyUtils.cs
@@ -2,9 +2,22 @@
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Mono_Ether {
+    public enum SliderType {
+        MASTER, SFX, MUSIC
+    }
     internal static class MyUtils {
         public static Rectangle RectangleF(float x, float y, float width, float height) => new Rectangle((int)x, (int)y, (int)width, (int)height);
         public static Vector2 Size(this Texture2D texture) => new Vector2(texture.Width, texture.Height);
         public static void DrawStringCentered(this SpriteBatch batch, SpriteFont font, string text, Vector2 position, Color color) => batch.DrawString(font, text, position, color, 0f, font.MeasureString(text) / 2f, 1f, 0, 0);
+        public static string SliderTypeToName(SliderType type)
+        {
+            return type switch
+            {
+                SliderType.MASTER => "Master volume",
+                SliderType.SFX => "Sound volume",
+                SliderType.MUSIC => "Music volume",
+                _ => "asdf"
+            };
+        }
     }
 }
