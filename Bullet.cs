959e9c6bc3b07485c3c4e8851f083e8ec5e0d1f0
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-12-23 17:33:23 +0000

    Add template bullet class

---
+++ Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mono_Ether {
+    public class Bullet : Entity
+    {
+        public static Texture2D BulletTexture;
+        public static Texture2D BulletGlowTexture;
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+        public void LoadContent(ContentManager content)
+        {
+            BulletTexture = content.Load<Texture2D>("Textures/GameScreen/Bullet");
+            BulletGlowTexture = content.Load<Texture2D>("Textures/GameScreen/BulletGlow");
+        }
+        public void UnloadContent()
+        {
+            BulletTexture = null;
+            BulletGlowTexture = null;
+        }
+    }
+}
